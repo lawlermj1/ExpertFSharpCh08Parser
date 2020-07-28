f15da949b546482140c614a11c830f4a71f0a108
Matthew Lawler <lawlermj1@gmail.com>
2020-07-28 12:50:24 +1000

    Initial Commit

---
+++ ExpertFSharpCh08Parser.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open Polynomial 
+
+[<EntryPoint>]
+let main argv =
+
+    let exString = @"x^5 - 2x^3 + 20"
+    printfn "Example tokenize x^5 - 2x^3 + 20 = %A " (tokenize exString) 
+    printfn " "  
+
+    printfn "Example parse 1+3 = %A " (parse "1+3") 
+    printfn " "  
+
+    printfn "Example parse 2x^2+3x+5 = %A " (parse "2x^2+3x+5") 
+    printfn " "  
+
+    printfn "Example parse x^5 - 2x^3 + 20 = %A " (parse exString) 
+    printfn " "  
+
+    printfn " "  
+    printfn "All finished from ExpertF#Ch08Parser" 
+    0 // return an integer exit code
---
+++ ExpertFSharpCh08Parser.fsx
@@ -0,0 +1,90 @@
+//  ExpertFSharpCh08Parser.fsx
+
+open System 
+
+//   tokenizer 
+type Token = 
+    | ID of string 
+    | INT of int
+    | HAT 
+    | PLUS 
+    | MINUS 
+
+let regex s = System.Text.RegularExpressions.Regex(s) 
+let tokenR = regex @"((?<token>(\d+|\w+|\^|\+|\-))\s*)*"
+
+let tokenize (s : string) =
+    [for x in tokenR.Match(s).Groups.["token"].Captures do
+        let token = 
+            match x.Value with 
+            | "^" -> HAT 
+            | "-" -> MINUS 
+            | "+" -> PLUS
+            | s when System.Char.IsDigit s.[0] -> INT (int s)
+            | s -> ID s 
+        yield token ]
+
+let exString = @"x^5 - 2x^3 + 20"
+tokenize exString ;;
+
+//  recursive descent parsing 
+type Term = 
+    | Term of int * string * int 
+    | Const of int 
+
+// "x^5 - 2x^3 + 20" = ex 
+let exTL = [Term (1,"x",5); Term(-2,"x",3); Const 20]
+
+type Polynomial = Term List 
+type TokenStream = Token List 
+
+let tryToken (src : TokenStream) = 
+    match src with 
+    | tok :: rest -> Some(tok, rest)
+    | _ -> None 
+
+let parseIndex src = 
+    match tryToken src with 
+    | Some (HAT, src) -> 
+        match tryToken src with 
+        | Some (INT num2, src) -> num2, src 
+        | _ -> failwith "expected an integer after '^'" 
+    | _ -> 1, src 
+
+let parseTerm src = 
+    match tryToken src with 
+    | Some (INT num, src) -> 
+        match tryToken src with 
+        | Some (ID id, src) -> 
+            let idx, src = parseIndex src 
+            Term (num, id, idx), src 
+        | _ -> Const num, src 
+    | Some (ID id, src) -> 
+        let idx, src = parseIndex src 
+        Term (1, id, idx), src 
+    | _ -> failwith "end of token stream in term"  
+
+// minus does not work 
+let rec parsePolynomial src = 
+    let t1, src = parseTerm src 
+    match tryToken src with 
+    | Some (PLUS, src) -> 
+        let p2, src = parsePolynomial src 
+        (t1 :: p2), src 
+    | Some (MINUS, src) -> 
+        let m2, src = parsePolynomial src 
+        (t1 :: m2), src 
+    | _-> [t1], src 
+
+let parse input = 
+    let src = tokenize input 
+    let result, src = parsePolynomial src
+    match tryToken src with 
+    | Some _ -> failwith "unexpected input at end of token stream!"  
+    | None -> result 
+
+parse "1+3" ;; 
+
+parse "2x^2+3x+5" ;; 
+
+parse exString ;;
